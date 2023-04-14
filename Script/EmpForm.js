52c802bf46689dbe814b4e89c7cfcfdf86c40a9f
SaiVenkat12 <gsaiv1001@gmail.com>
2023-04-14 21:55:23 +0530

    [Sai Venkat]- Added-Employee PayRoll data to json file

--- Script/EmpForm.js
+++ Script/EmpForm.js
@@ -1,28 +1,40 @@
 console.log("<<==Connected==>>")
-const submitForm=()=>{
-    const name=$('#Name').val()
-    const profile=$('input[name="profile"]:checked').val()
-    const gender=$('input[name="gender"]:checked').val()
-    const dept=$('input[name="dept"]:checked').val()
-    const salary=$('#salary').val()
-    const date=$('input[name="startdate"]:checked').val()
-    const note=$('#notes').val()
 
-    let reqData={
-        "Name":name,
-        "Profile":profile,
-        "Gender":gender,
-        "Department":dept,
-        "Salary":salary,
-        "Startdate":date,
-        "Notes":note,
+const submitForm = () => {
+    const name = $('#Name').val()
+    const profile = $('input[name="profile"]:checked').val()
+    const gender = $('input[name="gender"]:checked').val()
+
+    let deptArr=new Array();
+    (function(){
+        $('input[type="checkbox"]:checked').each(function(){
+            deptArr.push($(this).val());
+        })
+    })();
+     const dept = deptArr.toString();
+    
+    const salary = $('#salary').val()
+    const day = $('#day').val();
+    const month = $('#month').val();
+    const year = $('#year').val();
+    const note = $('#notes').val()
+    const date = day + ' ' + month + ' ' + year;
+
+    let reqData = {
+        "Name": name,
+        "Profile": profile,
+        "Gender": gender,
+        "Department": dept,
+        "Salary": salary,
+        "Startdate": date,
+        "Notes": note,
     }
     $.ajax({
-        type:'POST',
-        url:"http://localhost:3000/employees",
-        datatype:'application/json',
-        data:reqData,
-        success:function(){
+        type: 'POST',
+        url: "http://localhost:3000/employees",
+        datatype: 'application/json',
+        data: reqData,
+        success: function () {
             alert("employee data added Successfully");
         },
         error: function (error) {
